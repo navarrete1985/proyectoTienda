812c05bad6e4c93175de60f96766ae5384884bc5
Nacho <nacho_pena@hotmail.com>
2019-02-04 21:42:52 +0000

    Agragadas plantillas para tienda backend, tabulación y correcto funcionamiento

--- tienda/classes/tienda/controller/MainController.php
+++ tienda/classes/tienda/controller/MainController.php
@@ -12,7 +12,7 @@ class MainController extends Controller {
     
     function main() {
         // $this->checkIsLogged();
-        $this->getModel()->set('twigFile', 'index2.twig');
+        $this->getModel()->set('twigFile', 'product.twig');
         // $usuario = $this->getSesion()->getLogin()->get();
     }
 }
